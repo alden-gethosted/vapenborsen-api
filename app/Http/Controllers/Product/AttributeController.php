22cfb9fbeecd811ecbac644ef7042a57d8ce75ce
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-04-22 02:14:51 +0600

    Attribute Set

--- app/Http/Controllers/Product/AttributeController.php
+++ app/Http/Controllers/Product/AttributeController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\AttributeResource;
 use App\Models\Attribute;
 use App\Models\AttributeLink;
+use App\Models\AttributeValue;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class AttributeController extends Controller
@@ -35,28 +37,43 @@ class AttributeController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:191|unique:attributes,name',
             'attribute_sets_id' => 'required|numeric',
-            'product_categories_id' => 'required|array'
+            'product_categories_id' => 'required|array',
+            'attr_values' => 'required|array',
+            'is_filterable' =>  'required|boolean'
         ]);
         if ($validator->fails()) return response()->json($validator->errors(), config('naz.validation'));
 
-        try{
+        DB::beginTransaction();
+            try{
 
-            $table = new Attribute();
-            $table->name = $request->name;
-            $table->is_filterable = $request->is_filterable;
-            $table->attribute_sets_id = $request->attribute_sets_id;
-            $table->save();
-            $attributes_id = $table->id;
+                $table = new Attribute();
+                $table->name = $request->name;
+                $table->is_filterable = $request->is_filterable;
+                $table->attribute_sets_id = $request->attribute_sets_id;
+                $table->save();
+                $attributes_id = $table->id;
 
-            $categories = $request->product_categories_id;
+                $categories = $request->product_categories_id;
+                foreach ($categories as $cat_id){
+                    $attr_link = new AttributeLink();
+                    $attr_link->product_categories_id = $cat_id;
+                    $attr_link->attributes_id = $attributes_id;
+                    $attr_link->save();
+                }
 
-            foreach ($categories as $cat_id){
-                AttributeLink::updateOrCreate(['product_categories_id ' => $cat_id, 'attributes_id ' => $attributes_id]);
+                $attr_values = $request->attr_values;
+                foreach ($attr_values as $attr_value){
+                    $attr_val = new AttributeValue();
+                    $attr_val->name = $attr_value;
+                    $attr_val->attributes_id = $attributes_id;
+                    $attr_val->save();
+                }
+
+            }catch (\Exception $ex) {
+                DB::rollBack();
+                return response()->json(config('naz.db'), config('naz.db_error'));
             }
-
-        }catch (\Exception $ex) {
-            return response()->json(config('naz.db'), config('naz.db_error'));
-        }
+        DB::commit();
 
         return new AttributeResource($table);
     }
@@ -90,10 +107,13 @@ class AttributeController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:191|unique:attributes,name,'.$id,
             'attribute_sets_id' => 'required|numeric',
-            'product_categories_id' => 'required|array'
+            'product_categories_id' => 'required|array',
+            'attr_values' => 'required|array',
+            'is_filterable' =>  'required|boolean'
         ]);
         if ($validator->fails()) return response()->json($validator->errors(), config('naz.validation'));
 
+        DB::beginTransaction();
         try{
 
             $table = Attribute::find($id);
@@ -105,14 +125,28 @@ class AttributeController extends Controller
             $categories = $request->product_categories_id;
 
             AttributeLink::where('attributes_id', $id)->delete();
-
             foreach ($categories as $cat_id){
-                AttributeLink::updateOrCreate(['product_categories_id ' => $cat_id, 'attributes_id ' => $id]);
+                $attr_link = new AttributeLink();
+                $attr_link->product_categories_id = $cat_id;
+                $attr_link->attributes_id = $id;
+                $attr_link->save();
+            }
+
+            $attr_values = $request->attr_values;
+
+            AttributeValue::where('attributes_id', $id)->delete();
+            foreach ($attr_values as $attr_value){
+                $attr_val = new AttributeValue();
+                $attr_val->name = $attr_value;
+                $attr_val->attributes_id = $id;
+                $attr_val->save();
             }
 
         }catch (\Exception $ex) {
+            DB::rollBack();
             return response()->json(config('naz.db'), config('naz.db_error'));
         }
+        DB::commit();
 
         return new AttributeResource($table);
     }
