11dd8e58c3042e3a77bd661dc6bcd6d03792a596
rokibulislam <rrcucse.11@gmail.com>
2021-04-26 21:52:42 +0600

    add company api

---
+++ app/Http/Resources/CompanyResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CompanyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'users_id' => $this->users_id,
+            'name' => $this->name,
+            'logo' => isset( $this->logo ) ? $this->logo : '',
+            'description' => isset( $this->description ) ? $this->description : '', 
+            'contact' => isset( $this->contact ) ? $this->contact : '',
+            'contact_person' => isset( $this->contact_person ) ? $this->contact_person : '',
+            'website' => isset( $this->website ) ? $this->website : '',
+            'status' => $this->status
+        ];
+    }
+}
