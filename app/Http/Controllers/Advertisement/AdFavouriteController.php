f39b0e5624075666edcd2d1ee219fabb0db2e0f3
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-05-11 14:17:34 +0600

    Update AdFavouriteController.php

--- app/Http/Controllers/Advertisement/AdFavouriteController.php
+++ app/Http/Controllers/Advertisement/AdFavouriteController.php
@@ -4,36 +4,57 @@ namespace App\Http\Controllers\Advertisement;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\AdsFavorites;
+use App\Http\Resources\AdFavouriteResource;
+use Illuminate\Support\Facades\Validator;
 
 class AdFavouriteController extends Controller
 {
 
-    public function index()
+    public function index( $users_id )
     {
-        //
+        try{
+            $adFavorite = AdsFavorites::where( 'users_id', $users_id )->orderBy('id', 'DESC')->get();
+            if(!$adFavorite)
+                return response()->json(config('naz.n_found'), config('naz.not_found'));
+        } catch (\Exception $ex) {
+            return response()->json(config('naz.db'), config('naz.db_error'));
+        }
+
+        return AdFavouriteResource::collection($adFavorite);
     }
 
 
-    public function store(Request $request)
+    public function store( $users_id, Request $request )
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'ad_id' => 'required|exists:ads,id',
+        ]);
+
+        if ($validator->fails()) return response()->json($validator->errors(), config('naz.validation'));
+
+        try {
+            $adFavourite           = new AdsFavorites();
+            $adFavourite->ads_id   =  $request->ad_id;
+            $adFavourite->users_id =  $users_id;
+
+            $adFavourite->save();
+        } catch (\Exception $ex) {
+            return response()->json(config('naz.db'), config('naz.db_error'));
+        }
+
+        return new AdFavouriteResource($adFavourite);
     }
 
 
-    public function show($id)
+    public function destroy( $users_id, $id )
     {
-        //
-    }
+        try{
+            AdsFavorites::where( 'users_id', $users_id )->where('id', $id)->delete();
+        }catch (\Exception $ex) {
+            return response()->json(config('naz.db'), config('naz.db_error'));
+        }
 
-
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-
-    public function destroy($id)
-    {
-        //
+        return response()->json(config('naz.del'));
     }
 }
