d6f9a573a01c13c17aad1752b217b6eee34934b4
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-05-19 15:27:32 +0600

    mail test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Mail\TestMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    try {
+        Mail::to('wall.mate@gmail.com')->send(new TestMail('It works!'));
+    } catch (\Exception $ex) {
+        //dd($ex);
+    }
     return view('welcome');
 });
