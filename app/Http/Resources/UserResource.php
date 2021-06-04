185f0f3a835e9fcf83d19f74d9d54d260d3643c7
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-06-04 11:32:42 +0600

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,6 +18,7 @@ class UserResource extends JsonResource
             'id'            => $this->id,
             'name'          => $this->name,
             'email'       => $this->email,
+            'types'       => $this->types,
             'website'       => $this->website,
             'status'       => $this->status ? 'Active':'Inactive',
             'address'       => $this->address,
