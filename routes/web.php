90a5fc0501c3381f8627c0f5d841f2b03cdb891a
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-06-25 11:05:45 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     try {
-        $dd = Mail::to('wall.mate@gmail.com')->send(new TestMail('It works!'));
-        dd($dd);
+        Mail::to('wall.mate@gmail.com')->send(new TestMail('It works!'));
+        //dd($dd);
     } catch (\Exception $ex) {
         dd($ex);
     }
