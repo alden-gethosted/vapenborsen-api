940da50c6ad35cb9a5549eb7a8dfc8df103236ae
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-06-29 12:11:54 +0600

    undo

--- app/Http/Controllers/Product/CategoryController.php
+++ app/Http/Controllers/Product/CategoryController.php
@@ -161,7 +161,7 @@ class CategoryController extends Controller
         }catch (\Exception $ex) {
             return response()->json(config('naz.db'), config('naz.db_error'));
         }
-        return ProductCategoriesResource::collection($table);
-        //return response()->json($table);
+        //return ProductCategoriesResource::collection($table);
+        return response()->json($table);
     }
 }
--- app/Models/ProductCategories.php
+++ app/Models/ProductCategories.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Http\Resources\ProductCategoriesResource;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -66,11 +65,11 @@ class ProductCategories extends Model
 
     public function parent()
     {
-        return ProductCategoriesResource::collection($this->belongsTo('App\Models\ProductCategories','parents_id')->where('parents_id', null)->with('parent')->select('id','name', 'parents_id', 'icon'));
+        return $this->belongsTo('App\Models\ProductCategories','parents_id')->where('parents_id', null)->with('parent')->select('id','name', 'parents_id', 'icon');
     }
 
     public function children()
     {
-        return ProductCategoriesResource::collection($this->hasMany('App\Models\ProductCategories','parents_id')->with('children')->select('id','name', 'parents_id', 'icon'));
+        return $this->hasMany('App\Models\ProductCategories','parents_id')->with('children')->select('id','name', 'parents_id', 'icon');
     }
 }
