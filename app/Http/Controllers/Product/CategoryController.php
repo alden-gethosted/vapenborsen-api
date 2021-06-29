51b1fcded6b21942fab7bc63e928d221904a7831
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-06-29 12:06:46 +0600

    j

--- app/Http/Controllers/Product/CategoryController.php
+++ app/Http/Controllers/Product/CategoryController.php
@@ -157,7 +157,7 @@ class CategoryController extends Controller
 
     public function category_tree(){
         try{
-            $table = ProductCategories::with('parent', 'children')->get();
+            $table = ProductCategories::with('parent', 'children', 'attributeLinks')->select('id','name', 'parents_id', 'icon')->get();
         }catch (\Exception $ex) {
             return response()->json(config('naz.db'), config('naz.db_error'));
         }
