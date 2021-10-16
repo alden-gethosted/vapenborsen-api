aa27b92ae96fb39f6cd1a9794a17eddebee8bb57
rokibulislam <rrcucse.11@gmail.com>
2021-10-17 05:01:11 +0600

    update email text

--- resources/views/welcomenewuser.blade.php
+++ resources/views/welcomenewuser.blade.php
@@ -9,7 +9,20 @@
 </head>
 
 <body>
-    <h1> Welcome New User </h1>
+    <h2> Velkommen til vår nye plattform! </h>
+    <h4> Nyttig å vite før Du får gleden av å utforske Våpenbørsen AS. </h4>
+    <p> For at dette nettstedet skal bestå og være en seriøs representert aktør og flere skal få gleden av å bruke nettstedet, er det viktig at du leser og forstår våre Vilkår & Betingelser da det annonseres Våpen & Våpendeler på nettstedet. Våpen og våpendeler kontrolleres av Norsk Politi, og krever godkjennelse før de skifter eier eller erverves. Alle annonser blir sjekket og våpen/ våpendeler definert som ulovlig, eller våpen og deler som ikke oppfyller våpenloven fører til utestengelse fra nettstedet. </p>
+    <h4> Nyttige tips & informasjon før bruk!  </h4>
+    <ul>
+        <li> Felt & Våpendepotet denne tjenesten er ¨gratis! ¨ Her finner du alt av smådeler & tilbehør til våpen, ladeutstyr, jaktklær, skytterklær, futteraler, paintball utstyr, og blinker med mere. </li>
+        <li> Nettstedet Er laget for å samle totalmarkedet og med interesse for Miljøet, Sporten, Markedet, jo flere som starter å bruke nettstedet jo bedre for oss alle. </li>
+        <li> Din Bedrift Har egen innloggingsfunksjon på vårt nettsted. På Våpenbørsen strekker Dere ut ¨direkte¨ til et bredt interessespekter av kunder i markedet som er samlet på et sted. Bedriften kan annonsere eller reklamere for produktnyheter og de kategoriene som finnes på vår nettside.</li>
+        <li> Årlige Gavekort Vi trekker 3 gavekort hele to ganger i året til våre betalende kunder, før jaktsesong og før jul. (Verdi 1000kr per stk.) </li>
+    </ul>
+    <p> Velkommen til en nyvinning i markedet og takk for at du velger å bruke Våpenbørsen! Del gjerne denne nyheten med flere som har bruk for tjenesten vi dekker.  </p>
+    <br/>
+    <p> FaceBook Våpenborse As </p>
+
 </body>
 
 </html>
