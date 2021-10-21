3b3b7613dae2f39bf3d7a17bf46d15389996d491
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2021-10-21 17:34:59 +0600

    product seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(UserSeeder::class);
-        $this->call(PermissionSeeder::class);
-        $this->call(CategorySeeder::class);
+        //$this->call(UserSeeder::class);
+        //$this->call(PermissionSeeder::class);
+        //$this->call(CategorySeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
